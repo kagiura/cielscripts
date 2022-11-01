88727f64fb31fb7f3e307dd62030cbd24305ad93
yuuukun <58155530+kamishirorui@users.noreply.github.com>
2022-11-01 20:24:20 +0700

    added hashMasterlist

---
+++ dist/hashMasterlist.js
@@ -0,0 +1,30 @@
+function cs__hashMasterlist() {
+  $(`.item`).each(function () {
+    const $thisCard = $(this);
+    const $thisHeader = $thisCard.find("h2");
+    const thisTitle = $thisHeader
+      .text()
+      .replace(/[^A-z]/g, "")
+      .toLocaleLowerCase();
+    $thisCard.attr("data-story-name", thisTitle);
+  });
+
+  const hash = window.location.hash.replace("#", "").toLocaleLowerCase();
+  if (hash) {
+    const $thisCard = $(".item[data-story-name='" + hash + "']")
+      .first()
+      .clone()
+      .attr("style", null);
+    console.log($thisCard);
+    $("body").prepend(`<div class="cs__hash">
+    <p>Scroll down to see more stories!</p>
+    </div>`);
+    $(".cs__hash").append($thisCard);
+    $(".cs__hash").append(`<a href="#">Hide this section</a>`);
+    $(".cs__hash a").click(function () {
+      $(".cs__hash").slideToggle("slow");
+    });
+  }
+}
+
+cs__hashMasterlist();
--- dist/main.js
+++ dist/main.js
@@ -71,4 +71,119 @@ function cs__readingList() {
   });
 }
 
-cs__readingList();
+// cs__readingList();
+
+function cs__appendTable() {
+  $("table#info tbody").prepend(
+    "<tr><th>chapters</th><th>release</th><th>read</th></tr>"
+  );
+}
+
+// cs__appendTable();
+
+function cs__convertCharactersToIcons() {
+  $(".characters").each(function () {
+    try {
+      const $charactersDiv = $(this);
+      const characters = $charactersDiv
+        .html()
+        .toLowerCase()
+        .replace(/ /g, "")
+        .replace(/"/g, "")
+        .replace(/<divclass=/g, ",")
+        .replace(/><\/div>/g, "")
+        .split(",")
+        .filter((v) => v);
+      console.log(characters);
+      characters.forEach((c) => {
+        $charactersDiv.closest("div.item").addClass(c);
+      });
+      $charactersDiv.html(
+        characters.map((c) => '<div class="' + c + '"></div>').join("")
+      );
+    } catch {}
+  });
+}
+
+// cs__convertCharactersToIcons();
+
+function cs__translatedStatus() {
+  // utility function that just sets the classes for us
+  function setClassesTranslated($item, state) {
+    if (state) {
+      $item.addClass("tl");
+    } else {
+      $item.removeClass("tl");
+    }
+  }
+
+  // for each card on the page,
+  // *can be changed to a different selector if just checkbox isnt specific enough
+  $(`.item`).each(function () {
+    const $thisCard = $(this);
+    const hasTranslation = !!$thisCard.has("a[href]").length;
+    console.log(hasTranslation);
+    setClassesTranslated($thisCard, hasTranslation);
+  });
+}
+
+// cs__translatedStatus();
+
+function cs__hashMasterlist() {
+  // for each card on the page,
+  // *can be changed to a different selector if just checkbox isnt specific enough
+  $(`.item`).each(function () {
+    const $thisCard = $(this);
+    const $thisHeader = $thisCard.find("h2");
+    const thisTitle = $thisHeader
+      .text()
+      .replace(/[^A-z]/g, "")
+      .toLocaleLowerCase();
+    $thisCard.attr("data-story-name", thisTitle);
+  });
+
+  const hash = window.location.hash.replace("#", "").toLocaleLowerCase();
+  if (hash) {
+    const $thisCard = $(".item[data-story-name='" + hash + "']")
+      .first()
+      .clone()
+      .attr("style", null);
+    console.log($thisCard);
+    $("body").prepend(`<div class="cs__hash">
+    <p>Scroll down to see more stories!</p>
+    </div>`);
+    $(".cs__hash").append($thisCard);
+    $(".cs__hash").append(`<a href="#">Hide this section</a>`);
+
+    $(".cs__hash a").click(function () {
+      $(".cs__hash").slideToggle("slow");
+    });
+  }
+}
+
+// cs__hashMasterlist();
+
+// function qESEventChoose(e) {
+//   $(".q-esevent-sidelist *").removeClass("active");
+
+//   window.location.hash = e;
+
+//   $(".q-esevent-main > *").hide(0, "swing", function () {
+//     $("[onclick*='" + e + "']").addClass("active");
+//     $("#" + e).show();
+//     $(".q-esevent-main__wrapper").scrollTop(0);
+//   });
+// }
+// $(document).ready(function () {
+//   $(".q-esevent-sidelist h3").click(function () {
+//     $(this).next("div").slideToggle("fast");
+//   });
+
+//   const hash = window.location.hash?.replace("#", "");
+//   if (hash) {
+//     qESEventChoose(hash);
+//     $("[onclick*='" + hash + "']")
+//       .closest("div")
+//       .show();
+//   }
+// });
